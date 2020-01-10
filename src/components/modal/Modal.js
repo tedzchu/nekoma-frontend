e2ee6ed1cec49585969e3f86f4ce28fa9c1e8e79
tedzchu <tedchu4@gmail.com>
2020-01-09 17:21:53 -0800

    Create Modal component

---
+++ src/components/modal/Modal.js
@@ -0,0 +1,21 @@
+//
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const Modal = ({ isShowing, hide, title, content }) => isShowing ? ReactDOM.createPortal(
+  <React.Fragment>
+    <div className="modal-overlay"/>
+    <div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
+      <div className="modal">
+        <div className="modal-header">
+          <h2>{title}</h2>
+          <FontAwesomeIcon icon="times" data-dismiss="modal" onClick={hide} />
+        </div>
+        <div>{content}</div>
+      </div>
+    </div>
+  </React.Fragment>, document.body
+) : null;
+
+export default Modal;
---
+++ src/components/modal/useModal.js
@@ -0,0 +1,18 @@
+
+//https://upmostly.com/tutorials/modal-components-react-custom-hooks
+import { useState } from 'react';
+
+const useModal = () => {
+  const [isShowing, setIsShowing] = useState(false);
+
+  function toggle() {
+    setIsShowing(!isShowing);
+  }
+
+  return {
+    isShowing,
+    toggle,
+  }
+};
+
+export default useModal;
