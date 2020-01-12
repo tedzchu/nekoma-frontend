1e6197bedd9f699e5dfde958341df063921b335d
it-door <tedchu4@gmail.com>
2020-01-12 11:05:32 -0800

    Updates

---
+++ precache-manifest.9bac8745e7b5557def7312f6575aab1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c682ffa6eefcd4dc21a18b9db62e5832",
+    "url": "/nekoma-frontend/index.html"
+  },
+  {
+    "revision": "615a69a1a213dac13797",
+    "url": "/nekoma-frontend/static/css/main.b253ff34.chunk.css"
+  },
+  {
+    "revision": "3ba3c4d451c7b23e36b2",
+    "url": "/nekoma-frontend/static/js/2.4ee9f495.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/nekoma-frontend/static/js/2.4ee9f495.chunk.js.LICENSE"
+  },
+  {
+    "revision": "615a69a1a213dac13797",
+    "url": "/nekoma-frontend/static/js/main.5e0f0766.chunk.js"
+  },
+  {
+    "revision": "fa03a0597ac598a579ef",
+    "url": "/nekoma-frontend/static/js/runtime-main.395e9853.js"
+  }
+]);
