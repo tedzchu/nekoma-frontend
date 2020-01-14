2605c9f702c3f9c9c9cac9d3c48e66674bfd121f
tedzchu <tedchu4@gmail.com>
2020-01-13 18:05:14 -0800

    Getting product edit functionality in

---
+++ src/forms/products/EditProductForm.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+
+const EditProductForm = props => {
+  const categories = [
+    { name: "Lily", sku: "lil" },
+    { name: "Raphie", sku: "rph" },
+    { name: "Tiny", sku: "tny" }
+  ];
+  const [product, setProduct] = useState(props.currentProduct);
+
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+
+    setProduct({ ...product, [name]: value });
+  };
+
+  useEffect(() => {
+    setProduct(props.currentProduct);
+  }, [props]);
+
+  return (
+    <form
+      onSubmit={event => {
+        event.preventDefault();
+        console.log(product);
+        props.updateProduct(product.id, product);
+      }}
+    >
+      <div className="flex-row">
+        <div className="flex-large">
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            value={product.name}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="flex-large">
+          <label>Category</label>
+          <select
+            name="category"
+            value={product.category}
+            onChange={handleInputChange}
+          >
+            <option value="">Select a category</option>
+            {categories.map(category => (
+              <option key={category.sku} value={category.name}>
+                {category.name}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+      <button>Update product</button>
+      <button
+        onClick={() => props.cancelUpdate()}
+        className="button muted-button"
+      >
+        Cancel
+      </button>
+    </form>
+  );
+};
+
+export default EditProductForm;
--- src/views/products/Products.js
+++ src/views/products/Products.js
@@ -1,50 +1,137 @@
-import React, { useState } from 'react'
-import ProductTable from '../../tables/ProductTable'
-import AddProductForm from '../../forms/products/AddProductForm'
-import Header from '../../components/Header'
-import Modal from '../../components/modal/Modal'
-import useModal from '../../components/modal/useModal'
+import React, { useState } from "react";
+import ProductTable from "../../tables/ProductTable";
+import AddProductForm from "../../forms/products/AddProductForm";
+import Header from "../../components/Header";
+import Modal from "../../components/modal/Modal";
+import useModal from "../../components/modal/useModal";
+import EditProductForm from "../../forms/products/EditProductForm";
 
 const Products = () => {
   const productsData = [
-    { id: 1, sku: 'sku-2', name: 'product', category: 'cat', date: 'january', enabled: true, count: 29, restock: '01/09/2020' },
-    { id: 2, sku: 'sku-5', name: 'product2', category: 'keychain', date: 'february', enabled: false, count: 10, restock: '11/26/2019' },
-    { id: 3, sku: 'sku-1', name: 'product3', category: 'poster', date: 'march', enabled: true, count: 43, restock: '01/09/2020' },
-    { id: 4, sku: 'sku-4', name: 'product4', category: 'pin', date: 'april', enabled: true, count: 17, restock: '12/15/2019' },
-    { id: 5, sku: 'sku-6', name: 'product5', category: 'cat', date: 'may', enabled: false, count: 3, restock: '10/31/2019' }
-  ]
+    {
+      id: 1,
+      sku: "sku-2",
+      name: "product",
+      category: "cat",
+      date: "january",
+      enabled: true,
+      count: 29,
+      restock: "01/09/2020"
+    },
+    {
+      id: 2,
+      sku: "sku-5",
+      name: "product2",
+      category: "keychain",
+      date: "february",
+      enabled: false,
+      count: 10,
+      restock: "11/26/2019"
+    },
+    {
+      id: 3,
+      sku: "sku-1",
+      name: "product3",
+      category: "poster",
+      date: "march",
+      enabled: true,
+      count: 43,
+      restock: "01/09/2020"
+    },
+    {
+      id: 4,
+      sku: "sku-4",
+      name: "product4",
+      category: "pin",
+      date: "april",
+      enabled: true,
+      count: 17,
+      restock: "12/15/2019"
+    },
+    {
+      id: 5,
+      sku: "sku-6",
+      name: "product5",
+      category: "cat",
+      date: "may",
+      enabled: false,
+      count: 3,
+      restock: "10/31/2019"
+    }
+  ];
 
-  const [products, setProducts] = useState(productsData)
-  const {isShowing, toggle} = useModal()
+  const [products, setProducts] = useState(productsData);
+  const { isShowing, toggle } = useModal();
 
   const addProduct = product => {
-    product.id = products.length + 1
-    setProducts([...products, product])
-  }
+    product.id = products.length + 1;
+    setProducts([...products, product]);
+  };
 
   const deleteProduct = id => {
-    setProducts(products.filter(product => product.id !== id))
-  }
+    setProducts(products.filter(product => product.id !== id));
+  };
 
-  const addProductForm = <AddProductForm addProduct={addProduct} hide={toggle}/>
+  const [editing, setEditing] = useState(false);
+  const initialFormState = { id: null, name: "", category: "" };
+  const [currentProduct, setCurrentProduct] = useState(initialFormState);
+
+  const editRow = product => {
+    setEditing(true);
+    toggle();
+    setCurrentProduct({
+      id: product.id,
+      name: product.name,
+      category: product.category
+    });
+  };
+
+  const updateProduct = (id, updatedProduct) => {
+    setEditing(false);
+    toggle();
+    setProducts(
+      products.map(product => (product.id === id ? updatedProduct : product))
+    );
+  };
+
+  const cancelUpdate = () => {
+    setEditing(false);
+    toggle();
+  };
+
+  const addProductForm = (
+    <AddProductForm addProduct={addProduct} hide={toggle} />
+  );
+  const editProductForm = (
+    <EditProductForm
+      editing={editing}
+      cancelUpdate={cancelUpdate}
+      currentProduct={currentProduct}
+      updateProduct={updateProduct}
+    />
+  );
 
   return (
     <div className="full-container">
       <Header title="Products" back="/" />
+      <Modal
+        isShowing={isShowing}
+        hide={toggle}
+        title={editing ? "Editing " + currentProduct.name : "Add a new product"}
+        content={editing ? editProductForm : addProductForm}
+      />
       <div className="container">
         <button onClick={toggle}>Add new product</button>
-        <Modal
-          isShowing={isShowing}
-          hide={toggle}
-          title="Add a new product"
-          content={addProductForm}
-        />
         <div className="flex-large">
-          <ProductTable products={products} deleteProduct={deleteProduct} />
+          <ProductTable
+            products={products}
+            editRow={editRow}
+            deleteProduct={deleteProduct}
+          />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
