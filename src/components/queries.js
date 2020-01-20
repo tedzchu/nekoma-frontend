e7cac46a864885c1d3cff3fcfc3318bc3fd6112c
it-door <tedchu4@gmail.com>
2020-01-20 03:03:50 -0800

    AddProduct mutation and Cat subscriptions

--- src/components/queries.js
+++ src/components/queries.js
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag';
-
+// good example on auth header mutations: https://www.apollographql.com/docs/tutorial/mutations/
 export const PRODUCTS = gql`
-  query getProducts {
+  query {
     products {
       sku
       name
@@ -15,10 +15,48 @@ export const PRODUCTS = gql`
 `;
 
 export const CATEGORIES = gql`
-  query getCategories {
+  query {
     categories {
-      name
       id
+      name
+      sku_code
+      products {
+        sku
+      }
+    }
+  }
+`;
+
+// mutations: https://devhoot.ooo/build-a-react-graphql-app-with-apollo-client-powered-by-hasura-engine/
+export const ADD_PRODUCT = gql`
+  mutation($name: String!, $sku: String!, $count: Int!, $cat_id: Int!, $date_added: date!){
+    insert_products(
+      objects: [
+        {
+          name: $name,
+          sku: $sku,
+          count: $count,
+          category_id: $cat_id,
+          date_created: $date_added
+        }
+      ]
+    ) {
+      returning {
+        id
+      }
+    }
+  }
+`;
+
+export const CATEGORIES_SUBSCRIPTION = gql`
+  subscription {
+    categories {
+      id
+      name
+      sku_code
+      products {
+        sku
+      }
     }
   }
 `;
