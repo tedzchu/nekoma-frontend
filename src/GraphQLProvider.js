8f1b8b20ecc1ef2de358a98b9ef3981a66590457
it-door <tedchu4@gmail.com>
2020-01-20 03:04:34 -0800

    Move from Apollo Boost to Client for subscriptions

--- src/GraphQLProvider.js
+++ src/GraphQLProvider.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import ApolloClient from 'apollo-boost';
+import ApolloClient from 'apollo-client';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { useAuth0 } from './react-auth0-spa';
-import config from './auth_config.json';
+
+// Setup the network "links"
+import { WebSocketLink } from 'apollo-link-ws';
+import { HttpLink } from 'apollo-link-http';
+import { split, ApolloLink, Observable } from 'apollo-link';
+import { getMainDefinition } from 'apollo-utilities';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 // https://github.com/auth0-samples/auth0-javascript-samples/issues/79
 // https://github.com/vonkanehoffen/planning-alerts/blob/master/frontend-web/src/GraphQLProvider.js
 
-console.log(config);
 /**
  * Apollo Provider with Auth0 token.
  * @param children
@@ -20,18 +25,71 @@ export const GraphQLProvider = ({ children }) => {
     return <div>Loading...</div>;
   }
 
-  const client = new ApolloClient({
-    uri: config.graphQlEndpoint,
-    request: async operation => {
-      // Get token or get refreshed token
-      const token = isAuthenticated ? await getTokenSilently() : null;
-      operation.setContext({
-        headers: {
-          authorization: token ? `Bearer ${token}` : undefined
+  const wsLink = new WebSocketLink({
+    uri: 'ws://nekoma.herokuapp.com/v1/graphql',
+    options: {
+      reconnect: true,
+      connectionParams: async () => {
+        const token = isAuthenticated ? await getTokenSilently() : null;
+        return {
+          headers: {
+            authorization: token ? `Bearer ${token}` : undefined
+          }
         }
-      });
-      console.log(token);
-    }
+      }
+    },
+  });
+  
+  const httpLink = new HttpLink({
+    uri: 'https://nekoma.herokuapp.com/v1/graphql',
+  });
+  
+  const request = async (operation) => {
+    const token = isAuthenticated ? await getTokenSilently() : null;
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : undefined
+      }
+    });
+  };
+  
+  const requestLink = new ApolloLink((operation, forward) =>
+    new Observable(observer => {
+      let handle;
+      Promise.resolve(operation)
+        .then(oper => request(oper))
+        .then(() => {
+          handle = forward(operation).subscribe({
+            next: observer.next.bind(observer),
+            error: observer.error.bind(observer),
+            complete: observer.complete.bind(observer),
+          });
+        })
+        .catch(observer.error.bind(observer));
+  
+      return () => {
+        if (handle) handle.unsubscribe();
+      };
+    })
+  );
+
+  const link = split(
+    // split based on operation type
+    ({ query }) => {
+      const { kind, operation } = getMainDefinition(query);
+      return kind === 'OperationDefinition' && operation === 'subscription';
+    },
+    wsLink,
+    httpLink,
+  );
+
+  const client = new ApolloClient({
+    link: ApolloLink.from([
+      requestLink,
+      link,
+    ]),
+    cache: new InMemoryCache(),
+    // uri: 'https://nekoma.herokuapp.com/v1/graphql',
   });
 
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
